4d8caaf8ee8bc009353273dc740bf1bd728a4977
tanilhamdi <alparslantanil@gmail.com>
2025-06-12 11:34:28 +0300

    calismasi lazim

--- startup/vite.config.js
+++ startup/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     strictPort: false,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Yerel Node.js sunucu
+        target: 'https://beggtho-server-2sgz3ymvs-unrams-projects.vercel.app', // Yerel Node.js sunucu
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
